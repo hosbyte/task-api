8a4329582c579c2278cb4fdf3ae136e957c3dc68
hosbyte <hosbyte@gmail.com>
2026-08-29 18:02:56 +0330

    api route insert

---
+++ routes/api.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Http\Controllers\TaskController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::apiResource('tasks' , TaskController::class);
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
